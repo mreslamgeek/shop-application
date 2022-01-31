4553e3542ce7d0ec28121ca8188dc314c307289d
Eslam Mohammed <42094349+mreslamgeek@users.noreply.github.com>
2022-01-31 18:42:36 +0200

    http request "posting data on server"

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -61,17 +63,71 @@ class ProductsProvider with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  void addProduct(Product product) {
-    final newProduct = Product(
-        id: DateTime.now().toString(),
+  Future<void> addProduct(Product product) async {
+    final url = Uri.https(
+      'shop-app-project-id-default-rtdb.firebaseio.com',
+      '/products.json',
+    );
+
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'title': product.title,
+          'description': product.description,
+          'price': product.price,
+          'imageUrl': product.imageUrl,
+          'isFavorite': product.isFavorite,
+        }),
+      );
+
+      final newProduct = Product(
+        id: json.decode(response.body)['name'],
         title: product.title,
         description: product.description,
         price: product.price,
-        imageUrl: product.imageUrl);
-    _items.add(newProduct);
-    //_items.insert(0 , newProduct); // at the start of the list
-    notifyListeners();
+        imageUrl: product.imageUrl,
+      );
+      _items.add(newProduct);
+      notifyListeners();
+    } catch (error) {
+      print(error);
+      throw (error);
+    }
   }
+  /* Future<void> addProduct(Product product) {
+    final url = Uri.https(
+      'shop-app-project-id-default-rtdb.firebaseio.com',
+      '/products.json',
+    );
+
+    return http
+        .post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'price': product.price,
+        'imageUrl': product.imageUrl,
+        'isFavorite': product.isFavorite,
+      }),
+    )
+        .then((response) {
+      final newProduct = Product(
+        id: json.decode(response.body)['name'],
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+      );
+      _items.add(newProduct);
+      //_items.insert(0 , newProduct); // at the start of the list
+      notifyListeners();
+    }).catchError((error) {
+      print(error);
+      throw error;
+    });
+  } */
 
   Product findById(String id) => _items.firstWhere((prod) => prod.id == id);
 
